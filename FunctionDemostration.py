948e9b72220450ba1376a959b5640997e482dc28
Trần Bảo Toàn <49049899+tbtoanit@users.noreply.github.com>
2020-05-26 20:10:48 +0700

    The first release
    abcd

---
+++ FunctionDemostration.py
@@ -0,0 +1,15 @@
+# def additional(*nums):
+#     total = 0
+#     for i in nums:
+#         total = total+i
+#
+#     return total
+
+#Ham lamda
+#nhanDoi = lambda a:a*2
+
+#Su dung ham filter va bieu thuc lamda de tim kiem phan tu chan
+
+listSN = [1,2,3,4,5,6,7,8,9,10]
+listSC = list(filter(lambda a: a%2 == 0,listSN))
+print(listSC)
---
+++ XuLy.py
@@ -0,0 +1,6 @@
+import FunctionDemostration as ft
+#
+# g = ft.additional(1,2,3,4,5,5,6)
+# print(g)
+
+# print(ft.nhanDoi(20))
